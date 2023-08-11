85e4c074266054531c407cb791476b80e000a342
Justin <justinquach11214@gmail.com>
2023-08-11 03:28:13 -0400

    newimg

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -3,8 +3,8 @@ import { ChannelList, useChatContext } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
 
-import Blinkchaticon from '../assets/blinkchaticon.png'
-import Logouticon from '../assets/logout.png'
+import Blinkchaticon from '../assets/blinkchat.png'
+import Logouticon from '../assets/logoutnew.png'
 
 
 //stream gives access to api to search channels, send messages etc;
@@ -16,12 +16,12 @@ const SideBar = () => (
     <div className = "channel-list__sidebar">
         <div className = "channel-list__sidebar__icon1">
             <div className = "icon1__inner">
-                <img src = { Blinkchaticon } alt = "Blinkicon" width = "35" />
+                <img src = { Blinkchaticon } alt = "Blinkicon" width = "34" />
             </div>
         </div>
         <div className = "channel-list__sidebar__icon2">
             <div className = "icon1__inner">
-                <img src = { Logouticon } alt = "Logout" width = "35" />
+                <img src = { Logouticon } alt = "Logout" width = "29" />
             </div>
         </div>
     </div>
