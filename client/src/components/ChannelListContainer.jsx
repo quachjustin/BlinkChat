d3e410c735c2e921061fb146a4b772e91b37c5f7
Justin <justinquach11214@gmail.com>
2023-08-11 02:18:11 -0400

    added channelsearch
    -need to add teamchannelpreview

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -43,6 +43,16 @@ const ChannelListContainer = () => {
         <div className = "channel-list__list__wrapper">
             <CompanyHeader />
             <ChannelSearch />
+            <ChannelList 
+                filters = {{}}
+                channelRenderFilterFn = {() => {}}
+                List = {(listProps) => (
+                    <TeamChannelList
+                        {...listProps}
+                        type = "team"
+                    />
+                )}
+            />
         </div>
     </>
   )
