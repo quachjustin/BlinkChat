53366062a4311aef7e3fdd6311c73ea9dda34d7f
Justin <justinquach11214@gmail.com>
2023-08-14 14:29:40 -0400

    channellist for messages

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -61,6 +61,22 @@ const ChannelListContainer = () => {
                     />
                 )}
             />
+            <ChannelList 
+                filters = {{}}
+                channelRenderFilterFn = {() => {}}
+                List = {(listProps) => (
+                    <TeamChannelList
+                        {...listProps}
+                        type = "messaging"
+                    />
+                )}
+                Preview={(previewProps) => (
+                    <TeamChannelPreview
+                    {...previewProps}
+                    type = "messaging"
+                    />
+                )}
+            />
         </div>
     </>
   )
