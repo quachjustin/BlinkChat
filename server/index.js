6f0b0bfcc59dbb66eb4fdd32fd7e1bb33146e7b6
Justin <justinquach11214@gmail.com>
2023-08-14 19:26:57 -0400

    server

--- server/index.js
+++ server/index.js
@@ -4,15 +4,14 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.listen(PORT, () => console.log('Server is running on port ${PORT}'));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
