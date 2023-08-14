9cc816da7ef939638e7643b56e09a70672585a6d
Justin <justinquach11214@gmail.com>
2023-08-14 15:36:25 -0400

    signup/signup form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,25 +3,30 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
-import { ChannelListContainer, ChannelContainer } from './components';
+import { ChannelListContainer, ChannelContainer, Auth } from './components';
 
 import './App.css';
 
 const apiKey = 'yej74jgc3aeq';
 const client = StreamChat.getInstance(apiKey);
 
-const App = () => {
-  return (
-    <div className = "app__wrapper">
-        <Chat client = {client} theme = "team light">
-            <ChannelListContainer
+const authToken = false;
 
-            />
-            <ChannelContainer
-            />
-        </Chat>
-    </div>
-  );
+const App = () => {
+
+    if(!authToken) return <Auth />
+
+    return (
+        <div className = "app__wrapper">
+            <Chat client = {client} theme = "team light">
+                <ChannelListContainer
+
+                />
+                <ChannelContainer
+                />
+            </Chat>
+        </div>
+    );
 }
 
 export default App;
