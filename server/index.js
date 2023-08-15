76458a152eadb2d01c4ec722aba638b15964d608
Justin <justinquach11214@gmail.com>
2023-08-15 00:24:35 -0400

    client server connected

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 
+const authRoutes = require("./routes/auth.js");
+
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 require('dotenv').config();
 
@@ -14,4 +16,7 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.use('/auth', authRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
