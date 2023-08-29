cd7331991a61f22694220ecb85404435350733f4
Justin <justinquach11214@gmail.com>
2023-08-29 14:07:20 -0400

    Update index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -14,4 +14,4 @@ export {default as TeamMessage} from './TeamMessage';
 export {default as Auth} from './Auth';
 export {default as UserList} from './UserList';
 
-export {default as ResultsDropDown } from './ResultsDropdown'
+export {default as ResultsDropdown } from './ResultsDropdown'
